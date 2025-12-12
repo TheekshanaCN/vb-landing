e7e408c753a0571bd51cd49303474a15a75dc9bd
TheekshanaCN <theekshanachamodhya@gmail.com>
2025-12-12 12:53:48 +0530

    Test

--- components/header.tsx
+++ components/header.tsx
@@ -43,7 +43,7 @@ export function Header() {
 
         <div className="hidden md:flex items-center gap-4">
           <AnimatedButton
-            onClick={() => window.open("http://localhost:3001", "_blank")}
+            onClick={() => window.open("http://localhost:3000", "_blank")}
           >
             Start Free
           </AnimatedButton>
